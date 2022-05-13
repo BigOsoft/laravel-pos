41c1db1134257b3646b6f8008bdbaa6d58d0fec2
abdullahsuleman <abdullahsuleman61@gmail.com>
2022-05-13 17:56:22 +0500

    syntax miss

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,7 +17,7 @@
                         <select class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
                         @php
                             foreach($poss as $pos) {
-                                echo '<option>['.$pos->pos_id.'] '.$pos->store.' '.$pos->address_one.' '.$pos->address_two.'</option>'
+                                echo '<option>['.$pos->pos_id.'] '.$pos->store.' '.$pos->address_one.' '.$pos->address_two.'</option>';
                             }
                         @endphp
                         </select>
