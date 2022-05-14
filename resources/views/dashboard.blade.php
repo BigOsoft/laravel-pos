19d3fc8ef44bafc63ffb0c41423ad7b1cf866815
abdullahsuleman <abdullahsuleman61@gmail.com>
2022-05-14 15:28:36 +0500

    Add csrf field

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,6 +20,7 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     You're logged in!<br><br>
                     <form method="post" action="/public/report">
+                        {{ csrf_field() }}
                         <div class="inline-block relative w-64">
                             <select name="posLocation" class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
                             @php
